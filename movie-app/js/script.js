80474f07def506f4dde5079dc77363cc834ae424
amirhossein <nikuro-dev>
2022-09-13 02:56:09 +0430

    movie app started

---
+++ movie-app/js/script.js
@@ -0,0 +1,7 @@
+
+
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a0814a81d9e0ea8e164320078c18b3cb&page=1';
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=a0814a81d9e0ea8e164320078c18b3cb&query="';
